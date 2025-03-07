6ea6a858bb547655e36d4573e8a0a137905cf626
alexnoy <alexander.noy123@gmail.com>
2025-03-06 21:46:50 -0500

    create function remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        let index = array.indexOf(arg);
+        if (index == -1) {
+            continue;
+        }else {
+            while (index != -1) {
+                array.splice(index, 1);
+                index = array.indexOf(arg);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
