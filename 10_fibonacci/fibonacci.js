60821ff63c4a07235b76c0cff4bbfffc599ed360
alexnoy <alexander.noy123@gmail.com>
2025-04-13 22:50:53 -0400

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) return 'OOPS';
 
+    fibonacciSequence = [0,1];
+
+    for (let i = 2; i <= +number; i++) {
+        fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+    }
+
+    return fibonacciSequence[number];
 };
 
 // Do not edit below this line
