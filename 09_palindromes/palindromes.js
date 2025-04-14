d2d9adf2f331250b5860deba53eb4f5e5dc0b716
alexnoy <alexander.noy123@gmail.com>
2025-04-13 21:44:18 -0400

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const strippedString = string.toLowerCase().replaceAll(/[^a-z0-9]/g,'');
 
+    const reversedString = strippedString.split('').reverse().join('')
+
+    return strippedString === reversedString;
 };
 
 // Do not edit below this line
