8ab977e7212ba0d26e135d22719b29a69f716abf
alexnoy <alexander.noy123@gmail.com>
2025-04-14 21:02:09 -0400

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const addDeath = function (people) {
+    people.forEach((person) => {
+        if (!person.yearOfDeath) {
+            return person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+}
 
+const findTheOldest = function (people) {
+    addDeath(people);
+    people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+    return people[0];
 };
 
 // Do not edit below this line
